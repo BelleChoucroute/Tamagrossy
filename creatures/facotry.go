516f2a3baf4cdedf5da1fa9f0e5d0c643c4f3a4c
BelleChoucroute <48658922+BelleChoucroute@users.noreply.github.com>
2019-08-04 13:25:23 +0200

    🎉 Fisrt commit
    Random egg and items pop.

---
+++ creatures/facotry.go
@@ -0,0 +1,51 @@
+components {
+  id: "factory"
+  component: "/creatures/factory.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "moustmoust_factory"
+  type: "factory"
+  data: "prototype: \"/creatures/moustmoust/moustmoust.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "cubeurkhoppus_factory"
+  type: "factory"
+  data: "prototype: \"/creatures/cubeurkhoppus/cubeurkhoppus.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
---
+++ detritus/factory.go
@@ -0,0 +1,69 @@
+components {
+  id: "factory"
+  component: "/detritus/factory.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "battery_factory"
+  type: "factory"
+  data: "prototype: \"/detritus/battery.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "bandage_factory"
+  type: "factory"
+  data: "prototype: \"/detritus/bandage.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "tissu_factory"
+  type: "factory"
+  data: "prototype: \"/detritus/tissu.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
